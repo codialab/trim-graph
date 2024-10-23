044cd54cac46219ee608caa5b15674fdea397f7f
Peter Heringer <peter.heringer@ddz.de>
2024-10-23 15:44:29 +0200

    Add code for orientation of nodes in links

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use clap::Parser;
 use itertools::Itertools;
-use log;
 use rayon::iter::IntoParallelIterator;
 use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 use std::collections::HashSet;
@@ -37,6 +36,103 @@ fn set_number_of_threads(params: &Params) {
     );
 }
 
+fn get_paths(paths: Vec<&str>, paths_to_keep: Vec<String>) -> Vec<String> {
+    log::info!("Filtering paths");
+    let paths = paths
+        .into_par_iter()
+        .filter(|l| {
+            paths_to_keep.contains(
+                &l.split('\t')
+                    .nth(1)
+                    .expect("All paths should have names")
+                    .to_string(),
+            )
+        })
+        .map(|s| s.to_string())
+        .collect::<Vec<_>>();
+    paths
+}
+
+fn get_nodes_of_paths(paths: &Vec<String>) -> Vec<Vec<(String, bool)>> {
+    paths
+        .par_iter()
+        .map(|p| {
+            p.split('\t')
+                .nth(2)
+                .unwrap()
+                .split(',')
+                .map(|s| {
+                    let trimmed = s.trim();
+                    (
+                        trimmed[..trimmed.len() - 1].to_string(),
+                        trimmed.ends_with('+'),
+                    )
+                })
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>()
+}
+
+fn get_segments_to_keep(nodes_of_paths: &Vec<Vec<(String, bool)>>) -> HashSet<String> {
+    nodes_of_paths
+        .par_iter()
+        .map(|nodes| {
+            HashSet::from_iter::<HashSet<String>>(
+                nodes
+                    .clone()
+                    .into_iter()
+                    .unzip::<String, bool, HashSet<String>, HashSet<bool>>()
+                    .0,
+            )
+        })
+        .reduce(HashSet::new, |acc: HashSet<String>, set| {
+            acc.union(&set).map(|s| s.to_string()).collect()
+        })
+}
+
+fn filter_segments(segments: Vec<&str>, nodes_to_keep: HashSet<String>) -> Vec<&str> {
+    segments
+        .into_par_iter()
+        .filter(|n| {
+            nodes_to_keep.contains(n.split('\t').nth(1).expect("All nodes should have ids"))
+        })
+        .collect::<Vec<_>>()
+}
+
+fn get_links_to_keep(
+    nodes_of_paths: Vec<Vec<(String, bool)>>,
+) -> HashSet<((String, bool), (String, bool))> {
+    nodes_of_paths
+        .into_par_iter()
+        .map(|p| p.into_iter().tuple_windows().collect::<HashSet<(_, _)>>())
+        .reduce(HashSet::new, |acc, set| {
+            acc.union(&set)
+                .map(|s| (s.0.to_owned(), s.1.to_owned()))
+                .collect()
+        })
+}
+
+fn filter_links(
+    links: Vec<&str>,
+    edges_to_keep: HashSet<((String, bool), (String, bool))>,
+) -> Vec<&str> {
+    links
+        .into_par_iter()
+        .filter(|l| {
+            let fields = l.split('\t').collect::<Vec<_>>();
+            let edge = (
+                (fields[1].to_string(), fields[2].contains('+')),
+                (fields[3].to_string(), fields[4].contains('+')),
+            );
+            let rev_edge = (
+                (fields[3].to_string(), fields[4].contains('+')),
+                (fields[1].to_string(), fields[2].contains('+')),
+            );
+            edges_to_keep.contains(&edge) || edges_to_keep.contains(&rev_edge)
+        })
+        .collect::<Vec<_>>()
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     env_logger::init();
 
@@ -53,13 +149,13 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut links = Vec::new();
     let mut headers = Vec::new();
     for line in graph {
-        if line.starts_with("S") {
+        if line.starts_with('S') {
             segments.push(line);
-        } else if line.starts_with("L") {
+        } else if line.starts_with('L') {
             links.push(line);
-        } else if line.starts_with("P") {
+        } else if line.starts_with('P') {
             paths.push(line);
-        } else if line.starts_with("H") {
+        } else if line.starts_with('H') {
             headers.push(line);
         }
     }
@@ -73,89 +169,28 @@ fn main() -> Result<(), Box<dyn Error>> {
         None => paths
             .par_iter()
             .map(|l| {
-                l.split("\t")
-                    .skip(1)
-                    .next()
+                l.split('\t')
+                    .nth(1)
                     .expect("All paths should have names")
                     .to_string()
             })
             .collect(),
     };
 
-    log::info!("Filtering paths");
-    let paths = paths
-        .into_par_iter()
-        .filter(|l| {
-            paths_to_keep.contains(
-                &l.split("\t")
-                    .skip(1)
-                    .next()
-                    .expect("All paths should have names")
-                    .to_string(),
-            )
-        })
-        .collect::<Vec<_>>();
+    let paths = get_paths(paths, paths_to_keep);
 
     log::info!("Getting nodes to keep");
-    let nodes_of_paths = paths
-        .par_iter()
-        .map(|p| {
-            p.split("\t")
-                .skip(2)
-                .next()
-                .unwrap()
-                .split(",")
-                .map(|s| {
-                    let trimmed = s.trim();
-                    trimmed[..trimmed.len() - 1].to_string()
-                })
-                .collect::<Vec<_>>()
-        })
-        .collect::<Vec<_>>();
-    let nodes_to_keep = nodes_of_paths
-        .par_iter()
-        .map(|nodes| HashSet::from_iter(nodes.clone()))
-        .reduce(
-            || HashSet::new(),
-            |acc: HashSet<String>, set| acc.union(&set).map(|s| s.to_string()).collect(),
-        );
+    let nodes_of_paths = get_nodes_of_paths(&paths);
+    let nodes_to_keep = get_segments_to_keep(&nodes_of_paths);
 
     log::info!("Removing nodes");
-    let segments = segments
-        .into_par_iter()
-        .filter(|n| {
-            nodes_to_keep.contains(
-                n.split("\t")
-                    .skip(1)
-                    .next()
-                    .expect("All nodes should have ids"),
-            )
-        })
-        .collect::<Vec<_>>();
+    let segments = filter_segments(segments, nodes_to_keep);
 
     log::info!("Getting edges to keep");
-    let edges_to_keep = nodes_of_paths
-        .into_par_iter()
-        .map(|p| p.into_iter().tuple_windows().collect::<HashSet<(_, _)>>())
-        .reduce(
-            || HashSet::new(),
-            |acc, set| {
-                acc.union(&set)
-                    .map(|s| (s.0.to_string(), s.1.to_string()))
-                    .collect()
-            },
-        );
+    let edges_to_keep = get_links_to_keep(nodes_of_paths);
 
     log::info!("Removing edges");
-    let links = links
-        .into_par_iter()
-        .filter(|l| {
-            let fields = l.split("\t").collect::<Vec<_>>();
-            let edge = (fields[1].to_string(), fields[3].to_string());
-            let rev_edge = (fields[3].to_string(), fields[1].to_string());
-            edges_to_keep.contains(&edge) || edges_to_keep.contains(&rev_edge)
-        })
-        .collect::<Vec<_>>();
+    let links = filter_links(links, edges_to_keep);
 
     let mut out = std::io::BufWriter::new(std::io::stdout());
     for h in headers {
